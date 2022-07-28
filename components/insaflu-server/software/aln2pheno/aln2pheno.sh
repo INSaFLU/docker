cfd9127d0682071c0bbaee24d41b2b5bf69dada5
Daniel Sobral <daniel.sobral@insa.min-saude.pt>
2022-07-28 18:20:46 +0100

    Correct paths

--- components/insaflu-server/software/aln2pheno/aln2pheno.sh
+++ components/insaflu-server/software/aln2pheno/aln2pheno.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-eval "$(/software/extra_software/miniconda/bin/conda shell.bash hook)"
+eval "$(/software/miniconda2/bin/conda shell.bash hook)"
 conda activate aln2pheno
 export PATH=/software/aln2pheno/:$PATH
 algn2pheno $@
