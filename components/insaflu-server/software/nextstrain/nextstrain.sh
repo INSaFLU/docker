8ec06725e02ccf81e205ac78e28ebc9c9f0b1f0e
Daniel Sobral <daniel.sobral@insa.min-saude.pt>
2022-07-29 15:42:44 +0100

    Correct paths and other tweaks

--- components/insaflu-server/software/nextstrain/nextstrain.sh
+++ components/insaflu-server/software/nextstrain/nextstrain.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-eval "$(/software/extra_software/miniconda/bin/conda shell.bash hook)"
+eval "$(/software/miniconda2/bin/conda shell.bash hook)"
 conda activate nextstrain
 nextstrain $@
 conda deactivate
