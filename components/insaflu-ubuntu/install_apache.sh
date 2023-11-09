4b5c0689d4a5668cd5b8df99e75ccb0be8db331e
SantosJGND <dourado.jns@gmail.com>
2023-11-09 17:07:47 +0000

    work ubuntu branch

--- components/insaflu-ubuntu/install_apache.sh
+++ components/insaflu-ubuntu/install_apache.sh
@@ -4,15 +4,14 @@ set -e
 echo "Install Apache"
 apt update -y
 
-add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+apt-get install software-properties-common -y
+add-apt-repository ppa:ubuntugis/ppa && apt-get update
 apt-get install  apache2 apache2-utils apache2-dev libexpat1 ssl-cert python3 python3-pip -y
 apt-get install  libapache2-mod-wsgi-py3 -y
 
-a2enmod wsgi
-systemctl restart apache2
+service apache2 restart
 
 useradd apache
-echo `systemctl status apache2`
 
 #### create a apache
 # Create a group and user to run insaflu
@@ -23,7 +22,7 @@ echo "FIRST"
 
 ### apache server
 echo "Setup Apache httpd"
-usermod -a -G ${APP_USER} apache && mv /tmp_install/configs/insaflu.conf /etc/httpd/conf.d && rm /etc/httpd/conf.d/userdir.conf /etc/httpd/conf.d/welcome.conf && echo 'ServerName localhost' >> /etc/httpd/conf/httpd.conf && sed 's~</IfModule>~\n    AddType application/octet-stream .bam\n\n</IfModule>~' /etc/httpd/conf/httpd.conf > /etc/httpd/conf/httpd.conf_temp && mv /etc/httpd/conf/httpd.conf_temp /etc/httpd/conf/httpd.conf
+usermod -a -G ${APP_USER} apache && mv /tmp_install/configs/insaflu.conf /etc/apache2/conf.d && rm /etc/apache2/conf.d/userdir.conf /etc/apache2/conf.d/welcome.conf && echo 'ServerName localhost' >> /etc/apache2/conf/httpd.conf && sed 's~</IfModule>~\n    AddType application/octet-stream .bam\n\n</IfModule>~' /etc/apache2/conf/httpd.conf > /etc/apache2/conf/httpd.conf_temp && mv /etc/apache2/conf/httpd.conf_temp /etc/apache2/conf/httpd.conf
 if [ $? -ne 0 ]; then
     echo "Error installing apache"
     exit 1
