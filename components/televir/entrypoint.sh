d60c801e59443fbb0302c3ebecc5a7599aa9650f
SantosJGND <dourado.jns@gmail.com>
2024-11-15 12:37:01 +0000

    simplify config

--- components/televir/entrypoint.sh
+++ components/televir/entrypoint.sh
@@ -9,11 +9,10 @@ if [ "$1" = "move" ]; then
     
     cd insaflu_web/TELEVIR
     
-    /opt/venv/bin/python main.py --docker --envs --setup_conda --partial
-    /opt/venv/bin/python main.py --docker --envs --partial
-    
-    /opt/venv/bin/python main.py --docker --seqdl --partial
-    /opt/venv/bin/python main.py --docker --soft --partial
+    /opt/venv/bin/python main.py --docker --envs --setup_conda --seqdl --soft --partial
+    #/opt/venv/bin/python main.py --docker --envs --partial
+    #/opt/venv/bin/python main.py --docker --seqdl --partial
+    #/opt/venv/bin/python main.py --docker --soft --partial
     
     cp install_scripts/config.py /televir/mngs_benchmark/config.py
     
