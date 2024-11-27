8a06b93f294c566fe6220d9d762b8d80f31828c4
SantosJGND <dourado.jns@gmail.com>
2024-11-27 10:32:15 +0000

    separate concerns, install missing dependencies python

--- components/insaflu-ubuntu/install_website.sh
+++ components/insaflu-ubuntu/install_website.sh
@@ -8,17 +8,18 @@ echo `pip3 --version`
 
 
 #mkdir /insaflu_web && cd /insaflu_web && pip3 install Cython && git clone https://github.com/INSaFLU/INSaFLU.git && cd INSaFLU && pip3 install -r requirements.txt && pip3 install mod_wsgi-standalone && rm /etc/httpd/modules/mod_wsgi.so &&  ln -s /usr/local/lib64/python3.6/site-packages/mod_wsgi/server/mod_wsgi-py36.cpython-36m-x86_64-linux-gnu.so /etc/httpd/modules/mod_wsgi.so && mkdir -p /insaflu_web/INSaFLU/env && mv /tmp_install/configs/insaflu.env /insaflu_web/INSaFLU/.env && chown -R ${APP_USER}:${APP_USER} * && mkdir /var/log/insaFlu && chown -R ${APP_USER}:${APP_USER} /var/log/insaFlu
-mkdir /home/insaflu_web && cd /home/insaflu_web && git clone --branch metagenomics-ubuntu https://github.com/SantosJGND/INSaFLU.git && cd INSaFLU && pip3 install -r requirements.txt
+mkdir /home/insaflu_web && cd /home/insaflu_web
+git clone --branch metagenomics-ubuntu https://github.com/SantosJGND/INSaFLU.git #&& cd INSaFLU && pip3 install -r requirements.txt
 if [ $? -ne 0 ]; then
     echo "Error installing INSaFLU base"
     exit 1
 fi
 
-mkdir -p /insaflu_web/INSaFLU/env && mv /tmp_install/configs/insaflu.env /insaflu_web/INSaFLU/.env && chown -R ${APP_USER}:${APP_USER} * && mkdir /var/log/insaFlu
+#mkdir -p /insaflu_web/INSaFLU/env && mv /tmp_install/configs/insaflu.env /insaflu_web/INSaFLU/.env && chown -R ${APP_USER}:${APP_USER} * && mkdir /var/log/insaFlu
 
 
 ### Temp Directory /usr/lib/tmpfiles.d
-mv /tmp_install/configs/insaflu_tmp_path.conf /usr/lib/tmpfiles.d/insaflu_tmp_path.conf
+#mv /tmp_install/configs/insaflu_tmp_path.conf /usr/lib/tmpfiles.d/insaflu_tmp_path.conf
 
 
 #wget --no-check-certificate https://sourceforge.net/projects/gridengine/files/SGE/releases/8.1.9/sge-8.1.9.tar.gz/download -O sge-8.1.9.tar.gz; tar -zxvf sge-8.1.9.tar.gz
--- components/insaflu-ubuntu/install_website_dependencies.sh
+++ components/insaflu-ubuntu/install_website_dependencies.sh
@@ -18,7 +18,8 @@ apt install -y python3-pip
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+
+echo 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 
 
 ~/.pyenv/bin/pyenv install 3.8.3
