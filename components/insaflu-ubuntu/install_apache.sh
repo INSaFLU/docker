939a430981de9182e497fec8e86c97bdabb6e07e
SantosJGND <dourado.jns@gmail.com>
2024-11-27 13:35:23 +0000

    apache2 for ubuntu

--- components/insaflu-ubuntu/install_apache.sh
+++ components/insaflu-ubuntu/install_apache.sh
@@ -3,13 +3,13 @@ set -e
 
 echo "Install Apache"
 apt update -y
+apt-get install apache2 libapache2-mod-wsgi -y
 
 apt-get install software-properties-common -y
-add-apt-repository ppa:ubuntugis/ppa && apt-get update
-apt-get install  apache2 apache2-utils apache2-dev libexpat1 ssl-cert -y
-apt-get install  libapache2-mod-wsgi-py3 -y
-
-service apache2 restart
+#add-apt-repository ppa:ubuntugis/ppa && apt-get update
+#apt-get install  apache2 apache2-utils apache2-dev libexpat1 ssl-cert -y
+#apt-get install  libapache2-mod-wsgi-py3 -y
+#service apache2 restart
 
 useradd apache
 
@@ -21,7 +21,14 @@ echo `ls /usr/local/etc/httpd`
 
 ### apache server
 echo "Setup Apache httpd"
-usermod -a -G ${APP_USER} apache && mv /tmp_install/configs/insaflu.conf /etc/apache2/sites-available/000-default.conf && echo 'ServerName localhost' >> /etc/apache2/apache2.conf && sed 's~</IfModule>~\n    AddType application/octet-stream .bam\n\n</IfModule>~' /etc/apache2/mods-available/mime.conf > /etc/apache2/mods-available/mime.conf_temp && mv /etc/apache2/mods-available/mime.conf_temp /etc/apache2/mods-available/mime.conf
+usermod -a -G ${APP_USER} apache && mv /tmp_install/configs/insaflu.conf /etc/apache2/sites-available/000-default.conf
+echo 'ServerName localhost' >> /etc/apache2/apache2.conf
+sed 's~</IfModule>~\n    AddType application/octet-stream .bam\n\n</IfModule>~' /etc/apache2/mods-available/mime.conf > /etc/apache2/mods-available/mime.conf_temp
+mv /etc/apache2/mods-available/mime.conf_temp /etc/apache2/mods-available/mime.conf
+
+a2ensite 000-default.conf
+a2enmod wsgi
+
 if [ $? -ne 0 ]; then
     echo "Error installing apache"
     exit 1
