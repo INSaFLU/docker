c31f9b199f99d4a8511c61969da99a9e9e2c07fe
SantosJGND <dourado.jns@gmail.com>
2024-12-02 11:55:44 +0000

    wrap docker bash scripts, simplify docker file

--- components/insaflu-ubuntu/install_system_pkgs.sh
+++ components/insaflu-ubuntu/install_system_pkgs.sh
@@ -2,16 +2,49 @@
 set -e
 
 echo "Install System Packages"
-apt update -y
-apt install apt-utils dialog -y
-apt install make build-essential libssl-dev zlib1g-dev systemd -y
 
-apt install libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev -y
-apt-get install gfortran libopenblas-dev liblapack-dev -y
-apt-get install dos2unix parallel postgis postgresql-contrib postgresql  bash file binutils gzip git unzip wget default-jdk default-jre perl libperl-dev libtime-piece-perl libxml-simple-perl libdigest-perl-md5-perl libmodule-build-perl libfile-slurp-unicode-perl libtest-simple-perl gcc zlib1g libbz2-dev xz-utils cmake g++ autoconf bzip2 automake libtool -y
-apt install libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git apt-utils -y
+install_system_packages() {
+    apt update -y
+    if [ $? -ne 0 ]; then
+        echo "Error updating package list"
+        exit 1
+    fi
+    
+    apt install apt-utils dialog -y
+    if [ $? -ne 0 ]; then
+        echo "Error installing apt-utils and dialog"
+        exit 1
+    fi
+    
+    apt install make build-essential libssl-dev zlib1g-dev systemd -y
+    if [ $? -ne 0 ]; then
+        echo "Error installing build-essential packages"
+        exit 1
+    fi
+    
+    apt install libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev -y
+    if [ $? -ne 0 ]; then
+        echo "Error installing development libraries"
+        exit 1
+    fi
+    
+    apt-get install gfortran libopenblas-dev liblapack-dev -y
+    if [ $? -ne 0 ]; then
+        echo "Error installing Fortran and BLAS/LAPACK libraries"
+        exit 1
+    fi
+    
+    apt-get install dos2unix parallel postgis postgresql-contrib postgresql bash file binutils gzip git unzip wget default-jdk default-jre perl libperl-dev libtime-piece-perl libxml-simple-perl libdigest-perl-md5-perl libmodule-build-perl libfile-slurp-unicode-perl libtest-simple-perl gcc zlib1g libbz2-dev xz-utils cmake g++ autoconf bzip2 automake libtool -y
+    if [ $? -ne 0 ]; then
+        echo "Error installing various utilities and libraries"
+        exit 1
+    fi
+    
+    apt install libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git apt-utils nano -y
+    if [ $? -ne 0 ]; then
+        echo "Error installing additional development libraries"
+        exit 1
+    fi
+}
 
-if [ $? -ne 0 ]; then
-    echo "Error installing system packages"
-    exit 1
-fi
+install_system_packages
--- components/insaflu-ubuntu/install_website.sh
+++ components/insaflu-ubuntu/install_website.sh
@@ -1,47 +1,29 @@
+#!/bin/bash
+set -e
 
+setup_website() {
+    echo "Setup website code"
+    echo `which python3`
+    echo `python3 --version`
+    echo `pip3 --version`
+    
+    mkdir /insaflu_web && cd /insaflu_web
+    if [ $? -ne 0 ]; then
+        echo "Error creating or changing to /insaflu_web directory"
+        exit 1
+    fi
+    
+    git clone --branch metagenomics-ubuntu https://github.com/SantosJGND/INSaFLU.git && cd INSaFLU && pip3 install -r requirements.txt
+    if [ $? -ne 0 ]; then
+        echo "Error installing INSaFLU base"
+        exit 1
+    fi
+    
+    mkdir -p /insaflu_web/INSaFLU/env && mv /tmp_install/configs/insaflu.env /insaflu_web/INSaFLU/.env && chown -R ${APP_USER}:${APP_USER} * && mkdir /var/log/insaFlu && chown -R ${APP_USER}:${APP_USER} /var/log/insaFlu
+    if [ $? -ne 0 ]; then
+        echo "Error setting up environment and changing ownership"
+        exit 1
+    fi
+}
 
-### web site
-echo "Setup website code"
-echo `which python3`
-echo `python3 --version`
-echo `pip3 --version`
-
-
-#mkdir /insaflu_web && cd /insaflu_web && pip3 install Cython && git clone https://github.com/INSaFLU/INSaFLU.git && cd INSaFLU && pip3 install -r requirements.txt && pip3 install mod_wsgi-standalone && rm /etc/httpd/modules/mod_wsgi.so &&  ln -s /usr/local/lib64/python3.6/site-packages/mod_wsgi/server/mod_wsgi-py36.cpython-36m-x86_64-linux-gnu.so /etc/httpd/modules/mod_wsgi.so && mkdir -p /insaflu_web/INSaFLU/env && mv /tmp_install/configs/insaflu.env /insaflu_web/INSaFLU/.env && chown -R ${APP_USER}:${APP_USER} * && mkdir /var/log/insaFlu && chown -R ${APP_USER}:${APP_USER} /var/log/insaFlu
-mkdir /insaflu_web && cd /insaflu_web
-git clone --branch metagenomics-ubuntu https://github.com/SantosJGND/INSaFLU.git && cd INSaFLU && pip3 install -r requirements.txt
-if [ $? -ne 0 ]; then
-    echo "Error installing INSaFLU base"
-    exit 1
-fi
-
-
-
-mkdir -p /insaflu_web/INSaFLU/env && mv /tmp_install/configs/insaflu.env /insaflu_web/INSaFLU/.env && chown -R ${APP_USER}:${APP_USER} * && mkdir /var/log/insaFlu
-
-
-### Temp Directory /usr/lib/tmpfiles.d
-#mv /tmp_install/configs/insaflu_tmp_path.conf /usr/lib/tmpfiles.d/insaflu_tmp_path.conf
-
-
-#wget --no-check-certificate https://sourceforge.net/projects/gridengine/files/SGE/releases/8.1.9/sge-8.1.9.tar.gz/download -O sge-8.1.9.tar.gz; tar -zxvf sge-8.1.9.tar.gz
-
-#cd /insaflu_sge_source/sge-8.1.9/source
-#echo "INSTALLING SGE, BOOTSTRAP"
-#sh scripts/bootstrap.sh -no-java -no-jni && ./aimk -no-java -no-jni
-#echo Y | /insaflu_sge_source/sge/source/scripts/distinst -local -all -noexit
-#if [ $? -ne 0 ]; then
-#    echo "Error installing SGE"
-#    exit 1
-#fi
-
-#export SGE_ROOT=/opt/sge
-#groupadd -g 58 gridware && useradd -u 63 -g 58 -d ${SGE_ROOT} sgeadmin && chmod 0755 ${SGE_ROOT}
-
-#copy default files to the queues
-#mv /tmp_install/sge_default/default/ ${SGE_ROOT}/ && chown -R sgeadmin:gridware ${SGE_ROOT} && mv /tmp_install/sge_default/sun-grid-engine.sh /etc/profile.d/ && mv /tmp_install/sge_default/sgeexecd.p6444 /etc/init.d/ && mv /tmp_install/sge_default/sgemaster.p6444 /etc/init.d/ && mv /tmp_install/sge_default/root.cshrc /root/.cshrc && chmod a+x /etc/profile.d/sun-grid-engine.sh && rm -rf /insaflu_sge_source*
-#export PATH="/opt/sge/bin:/opt/sge/bin/lx-amd64:${PATH}"
-#
-### END SGE
-
-
+setup_website
