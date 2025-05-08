b537f6d4a33bebe7470114a2f8de9678d4ec81a4
SantosJGND <dourado.jns@gmail.com>
2025-05-08 14:08:19 +0100

    metaphlan conda install

--- components/televir/configs/config_install.py
+++ components/televir/configs/config_install.py
@@ -6,33 +6,37 @@ class TelevirLayout:
     install_rvdb = False
     install_virosaurus = True
     install_request_sequences = False
+    install_ribo16s = True
 
     # hosts ### CHECK HOST LIBRARY FILE FOR AVAILABLE HOSTS ###
     HOSTS_TO_INSTALL = [
         "hg38",
-        "sus_scrofa",
-        "aedes_albopictus",
-        "gallus_gallus",
-        "oncorhynchus_mykiss",
-        "salmo_salar",
-        "bos_taurus",
-        "neogale_vison",
-        "marmota_marmota",
-        "culex_pipiens",
-        "anas_platyrhynchos",
-        "pipistrellus_kuhlii",
-        "phlebotomus_papatasi",
-        "felis_catus",
-        "canis_lupus_familiaris",
-        "cyprinus_carpio",
+        #"sus_scrofa",
+        #"aedes_albopictus",
+        #"gallus_gallus",
+        #"oncorhynchus_mykiss",
+        #"salmo_salar",
+        #"bos_taurus",
+        #"neogale_vison",
+        #"marmota_marmota",
+        #"culex_pipiens",
+        #"anas_platyrhynchos",
+        #"pipistrellus_kuhlii",
+        #"phlebotomus_papatasi",
+        #"felis_catus",
+        #"canis_lupus_familiaris",
+        #"cyprinus_carpio",
     ]
 
     # host mappers
     install_bowtie2_remap = True
-    install_bowtie2_depletion = True
+    install_bowtie2_depletion = False
     install_bwa_host = True
+    install_bwa_filter= True
 
     # classification software.
+    install_metaphlan = True
+    install_voyager_viral = True
     install_centrifuge = True
     install_centrifuge_bacteria = False
     install_kraken2 = True
